b3697a660098908bda40ae517c6b1d50afe9d66e
Jener Rasmussen <git@jener.me>
2024-06-09 09:59:33 +0200

    Improve jenrik keymap for dztech/dz60v2
    Format keymap file.
    Switch ESC key with Grave key to aligh with actual keycaps.
    Add modifier to access delete key.

--- keyboards/dztech/dz60v2/keymaps/jenrik/keymap.c
+++ keyboards/dztech/dz60v2/keymaps/jenrik/keymap.c
@@ -10,19 +10,27 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
-        QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PSCR, KC_SCRL,
+        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_PSCR, KC_SCRL,
         KC_TAB,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, DB_TOGG,
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  MO(1),   KC_BSPC,          KC_RALT, KC_RGUI, XXXXXXX, MO(1),   KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,  MO(1),   KC_BSPC,          KC_RALT, KC_RGUI, XXXXXXX, MO(2),   KC_RCTL
     ),
 
     LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, QK_BOOT,
+        _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11 , KC_F12, _______, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______, QK_BOOT,
+        KC_ESC , _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN , KC_UP , KC_RIGHT, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______, _______, QK_BOOTLOADER,
+        _______, _______, _______,                   _______, _______, KC_DEL ,          _______, _______ , _______, _______, _______
+    ),
+
+    LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, ________, _______, _______, _______, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______ , _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN , KC_UP , KC_RIGHT, _______,          _______,
-        _______, _______, _______, _______, BL_DOWN, BL_TOGG, BL_UP,   BL_STEP, _______, _______, _______, _______, _______, QK_BOOTLOADER,
-        _______, _______, _______,                   _______, _______, _______,          _______, _______, _______, _______, _______
+        _______, _______, _______, _______, BL_DOWN, BL_TOGG, BL_UP,   BL_STEP, _______, _______, _______ , _______, _______, _______,
+        _______, _______, _______,                   _______, _______, _______,          _______, _______ , _______, _______, _______
     )
 };
 
